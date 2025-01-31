2009e0e3505ff81feafbdd5753265a9b2a157e7b
Romanenkov Dmitriy <215rd@mail.ru>
2025-01-31 22:27:13 +0300

    feat: companies table (god component)

--- src/widget/CompaniesTable/model/slice.ts
+++ src/widget/CompaniesTable/model/slice.ts
@@ -1,13 +1,17 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Company } from '../../../shared/types/shared';
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CompanyDTO } from '../../../shared/types/shared';
 import { mockCompanies } from '../../../shared/mock/companies';
 
+type CompanyStoreData = CompanyDTO & { isSelected: boolean };
+
 interface CompaniesState {
-  companies: Company[];
+  companies: Array<CompanyStoreData>;
+  itemsToRender: number;
 }
 
 const initialState: CompaniesState = {
-  companies: mockCompanies.slice(0, 3),
+  companies: mockCompanies.map((item) => ({ ...item, isSelected: false })),
+  itemsToRender: 20,
 };
 
 export const companiesSlice = createSlice({
@@ -15,20 +19,71 @@ export const companiesSlice = createSlice({
   initialState,
   selectors: {
     getAllCompanies: (store) => store.companies,
+    getItemsToRender: (store) => store.itemsToRender,
   },
   reducers: {
-    deleteCompany: (state, action: PayloadAction<{ id: string }>) => {
-      state.companies = state.companies.filter(
-        ({ id }) => id !== action.payload.id,
-      );
+    deleteSelectedCompanies: (state) => {
+      state.companies = state.companies.filter(({ isSelected }) => !isSelected);
     },
-    addCompany: (state, action: PayloadAction<Company>) => {
-      state.companies.push(action.payload);
+    addCompany: (state, action: PayloadAction<CompanyDTO>) => {
+      state.companies.push({ ...action.payload, isSelected: false });
+    },
+    setCompaniesToRender: (
+      state,
+      action: PayloadAction<Pick<CompaniesState, 'itemsToRender'>>,
+    ) => {
+      state.itemsToRender = action.payload.itemsToRender;
+    },
+    toggleSelectAll: (state) => {
+      const isAllRenderedCompaniesSelected = state.companies
+        .slice(0, state.itemsToRender)
+        .every(({ isSelected }) => isSelected);
+      for (
+        let i = 0;
+        i < Math.min(state.itemsToRender, state.companies.length);
+        i++
+      ) {
+        state.companies[i].isSelected = !isAllRenderedCompaniesSelected;
+      }
+    },
+    toggleSelectCompany: (
+      state,
+      action: PayloadAction<Pick<CompanyStoreData, 'id' | 'isSelected'>>,
+    ) => {
+      const company = state.companies.find(
+        (company) => company.id === action.payload.id,
+      );
+      if (company) {
+        company.isSelected = !action.payload.isSelected;
+      }
     },
   },
 });
 
-export const { addCompany, deleteCompany } = companiesSlice.actions;
-export const { getAllCompanies } = companiesSlice.selectors;
+const {
+  addCompany,
+  deleteSelectedCompanies,
+  setCompaniesToRender,
+  toggleSelectAll,
+  toggleSelectCompany,
+} = companiesSlice.actions;
+const { getAllCompanies, getItemsToRender } = companiesSlice.selectors;
 
+const getCompaniesToRender = createSelector(
+  [getAllCompanies, getItemsToRender],
+  (companies, itemsToRender) =>
+    itemsToRender >= companies.length
+      ? companies
+      : companies.slice(0, itemsToRender),
+);
+
+export {
+  addCompany,
+  deleteSelectedCompanies,
+  getAllCompanies,
+  getCompaniesToRender,
+  setCompaniesToRender,
+  toggleSelectAll,
+  toggleSelectCompany,
+};
 export default companiesSlice.reducer;
--- src/widget/CompaniesTable/ui/index.tsx
+++ src/widget/CompaniesTable/ui/index.tsx
@@ -1,24 +1,145 @@
-import { useAppDispatch, useAppSelector } from '../../../app/store/hooks';
-import { addCompany, deleteCompany, getAllCompanies } from '../model/slice';
+import { useCallback, useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../app/store';
+import { Sentinel } from '../../../shared/ui/Sentinel';
+import {
+  addCompany,
+  deleteSelectedCompanies,
+  getCompaniesToRender,
+  setCompaniesToRender,
+  toggleSelectAll,
+  toggleSelectCompany,
+} from '../model/slice';
+import { useVirtualScroll } from '../../../shared/hooks/useVirtualScroll';
+import { formDataToObject } from '../../../shared/utils/formDataToObject';
+import { isCompanyDTO } from '../../../shared/guards/isCompanyDTO';
+
+const ITEMS_IN_LOAD_AMOUNT = 10;
+const ROW_HEIGHT = 40;
 
 export const CompaniesTable = () => {
   const dispatch = useAppDispatch();
-  const companies = useAppSelector(getAllCompanies);
-  const addNewCompany = () =>
-    dispatch(addCompany({ address: 'Адрес', id: 'asd', name: 'Имя' }));
-  const deleteNewCompany = () => {
-    dispatch(deleteCompany({ id: 'asd' }));
+  const companies = useAppSelector(getCompaniesToRender);
+  const totalCompaniesLength = useAppSelector(
+    (store) => store.companies.companies.length,
+  );
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const getContainer = useCallback(() => containerRef.current, []);
+  const loadMore = () => {
+    dispatch(
+      setCompaniesToRender({
+        itemsToRender: companies.length + ITEMS_IN_LOAD_AMOUNT,
+      }),
+    );
   };
+  const { startIndex, endIndex, totalHeight, offset } = useVirtualScroll({
+    getScrollElement: getContainer,
+    itemHeight: ROW_HEIGHT,
+    totalItems: companies.length,
+  });
+
+  const itemsToRender = companies.slice(startIndex, endIndex + 1);
+
+  const isAllChecked =
+    companies.every(({ isSelected }) => isSelected) && companies.length !== 0;
 
   return (
-    <div>
-      {companies.map(({ name, id, address }) => (
-        <p key={id}>
-          {name}, {id}, {address}
-        </p>
-      ))}
-      <button onClick={addNewCompany}>Добавить</button>
-      <button onClick={deleteNewCompany}>Убавить</button>
-    </div>
+    <>
+      <div
+        style={{
+          height: 600,
+          overflow: 'auto',
+          width: '620px',
+          position: 'relative',
+        }}
+        ref={containerRef}
+      >
+        <div
+          style={{
+            height: totalHeight,
+            position: 'relative',
+          }}
+        >
+          <table
+            style={{
+              position: 'absolute',
+              top: offset,
+              width: '100%',
+              // translate: `0 ${offset}px`,
+            }}
+          >
+            <thead
+              style={{
+                position: 'sticky',
+                top: 0,
+                translate: `0 0px`,
+                background: 'rebeccapurple',
+                color: 'white',
+              }}
+            >
+              <tr style={{ height: ROW_HEIGHT }}>
+                <th style={{ width: '50px' }}>
+                  <input
+                    type="checkbox"
+                    checked={isAllChecked}
+                    onChange={() => dispatch(toggleSelectAll())}
+                  />
+                </th>
+                <th style={{ width: '275px' }}>Название</th>
+                <th style={{ width: '275px' }}>Адрес</th>
+              </tr>
+            </thead>
+            <tbody>
+              {itemsToRender.map(({ name, id, address, isSelected }) => (
+                <tr style={{ height: ROW_HEIGHT }} key={id}>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={isSelected}
+                      onChange={() =>
+                        dispatch(toggleSelectCompany({ id, isSelected }))
+                      }
+                    />
+                  </td>
+                  <td>{name}</td>
+                  <td>{address}</td>
+                </tr>
+              ))}
+            </tbody>
+            {companies.length < totalCompaniesLength && (
+              <Sentinel
+                getIntersectionContainer={getContainer}
+                onInView={loadMore}
+              />
+            )}
+          </table>
+        </div>
+      </div>
+
+      <button
+        onClick={() => dispatch(deleteSelectedCompanies())}
+        disabled={!companies.some(({ isSelected }) => isSelected)}
+      >
+        Удалить выбранные
+      </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const data = {
+            ...formDataToObject(formData),
+            id: Date.now().toString(),
+          };
+
+          if (isCompanyDTO(data)) {
+            dispatch(addCompany(data));
+          }
+        }}
+      >
+        <input name="name" type="text" />
+        <input name="address" type="text" />
+        <button>Добавить</button>
+      </form>
+    </>
   );
 };
