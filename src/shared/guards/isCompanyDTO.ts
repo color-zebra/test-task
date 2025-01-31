2774295f52b3620242882b398bc503c7b39dc374
Romanenkov Dmitriy <215rd@mail.ru>
2025-01-31 22:25:40 +0300

    feat: companyDTO typeguard

---
+++ src/shared/guards/isCompanyDTO.ts
@@ -0,0 +1,14 @@
+import { CompanyDTO } from '../types/shared';
+
+export const isCompanyDTO = (
+  obj: Record<string, unknown>,
+): obj is CompanyDTO => {
+  return (
+    'name' in obj &&
+    typeof obj.name === 'string' &&
+    'address' in obj &&
+    typeof obj.address === 'string' &&
+    'id' in obj &&
+    typeof obj.id === 'string'
+  );
+};
